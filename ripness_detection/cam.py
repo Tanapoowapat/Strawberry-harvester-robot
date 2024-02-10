55bb6162cbb26492f905b3c7e72f5820f2f673f6
Tanapoowpat <tanapoowapat@gmail.com>
2024-02-10 17:27:49 +0700

    modified:   ripness_detection/cam.py

--- ripness_detection/cam.py
+++ ripness_detection/cam.py
@@ -1,51 +1,54 @@
-import cv2
-from ultralytics import YOLO
 import time
-prev_frame_time = 0
-new_frame_time = 0
-
-global WINDOW_TITLE
-WINDOW_TITLE = "Strawberry Ripeness Detection"
-#LOAD MODEL
-model = YOLO('model/detection/best.pt')
-#LOAD WEBCAM 1 OR 0 for default webcam
-cap = cv2.VideoCapture(0)
-#READ THE FRAME
-if cap.isOpened():
-    try:
-        
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                print("Error: Could not read frame.")
-                break
-
-            
-            #PROCESS THE FRAME
-            results = model(frame, stream=True, half=True, conf=0.9)
-
-            #CALCULATE FPS
-            new_frame_time = time.time() 
-            fps = 1/(new_frame_time-prev_frame_time) 
-            prev_frame_time = new_frame_time 
-            fps = int(fps) 
-            fps = str(fps)
-
-            cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX , 3, (100, 255, 0), 3, cv2.LINE_AA) 
+import cv2
 
 
-            #DISPLAY THE FRAME
-            for result in results:
-                print(result)
-                frame = result.plot()
-                cv2.imshow('frame', frame)
+window_title = "USB Camera"
 
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-    except Exception as e:
-        print(e)
-    finally:
-        cap.release()
-        cv2.destroyAllWindows()
-else:
-    print("Error: Could not open webcam.")
+# ASSIGN CAMERA ADRESS to DEVICE HERE!
+pipeline = " ! ".join(["v4l2src device=/dev/video0",
+                       "video/x-raw, width=640, height=480, framerate=30/1",
+                       "videoconvert",
+                       "video/x-raw, format=(string)BGR",
+                       "appsink"
+                       ])
+
+def show_camera():
+    print('Start Reading Camera...')
+    video_capture = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+    #Init Mask
+    mask = cv2.imread('mask.png')
+    #Reize Mask to H 480 W 640
+    mask = cv2.resize(mask, (640, 480))
+    prev_frame_time = 0
+    new_frame_time = 0
+    if video_capture.isOpened():
+        try:
+            while True:
+                _, frame = video_capture.read()
+                #Bitwise And Mask
+                frame = cv2.bitwise_and(frame, mask)
+                
+                #CALCULATE FPS
+                new_frame_time = time.time()
+                fps = 1/(new_frame_time-prev_frame_time)
+                prev_frame_time = new_frame_time
+                fps = int(fps)
+                print(f'FPS :  {str(fps)}')
+                #Draw Rectangle x1 300 x2 340 y1 0 y2 480 
+                cv2.rectangle(frame, (300,0), (340,480), (0, 255, 0), 2)
+                cv2.imshow(window_title, frame)
+                keyCode = cv2.waitKey(10) & 0xFF
+                # Stop the program on the ESC key or 'q'
+                if keyCode == 27 or keyCode == ord('q'):
+                    break
+        except Exception as e:
+            print(e)
+        finally:
+            video_capture.release()
+            cv2.destroyAllWindows()
+    else:
+        print("Error: Unable to open camera")
+
+
+if __name__ == "__main__":
+    show_camera()
