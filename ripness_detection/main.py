99f64bd6ab75735e86ad9ac05110874e43752d32
Tanapoowpat <tanapoowapat@gmail.com>
2024-02-10 17:47:40 +0700

    modified:   ripness_detection/main.py 	deleted:    ripness_detection/usb-camera-gst.py

--- ripness_detection/main.py
+++ ripness_detection/main.py
@@ -1,52 +1,59 @@
-import cv2
 import time
-from process import load_model, load_webcam, process_frame
-from utils.utils import resize_mask
-
-# Constants Model&Frame
-WEBCAM_DIVICE = 0 #ใช้ Webcam เปลี่ยนตรงนี้เป็น 0
-model_path = "./model/segment/best.pt"
-#FRAME_SIZE = (640, 640)
+from process import find_strawberry
+import cv2
+from ultralytics import YOLO
 
 
-def main():
-    print("Load Model...")
-    model = load_model(model_path)
-    cap = load_webcam(WEBCAM_DIVICE)
-    if not cap.isOpened():
-        print("Error: Could not open webcam.")
-        exit()
-    
-    # Constants FPS
+window_title = "USB Camera"
+
+# ASSIGN CAMERA ADRESS to DEVICE HERE!
+pipeline = " ! ".join(["v4l2src device=/dev/video0",
+                       "video/x-raw, width=640, height=480, framerate=30/1",
+                       "videoconvert",
+                       "video/x-raw, format=(string)BGR",
+                       "appsink"
+                       ])
+
+def show_camera(model):
+    print('Start Reading Camera...')
+    video_capture = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
     prev_frame_time = 0
     new_frame_time = 0
-
-    COUNT = 0
-    while True:
-        ret, frame = cap.read()
-
-        if not ret:
-            print("Error: Could not read frame.")
-            break
-
-        # Process the frame
-        frame, ripness, box = process_frame(frame, model)
-        if ripness == "Full Ripe" and box is not None:
-            COUNT += 1
-            print(f"Count: {COUNT}")
-            time.sleep(10)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
-        new_frame_time = time.time()
-        fps = 1 / (new_frame_time - prev_frame_time)
-        prev_frame_time = new_frame_time
-        fps = str(int(fps))
-        cv2.putText(frame, "FPS: " + fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
-        cv2.imshow("frame", frame)
+    if video_capture.isOpened():
+        try:
+            while True:
+                _, frame = video_capture.read()
+                results = model(frame, stream=True, conf=0.9, half=True, device=0)  
+                #CALCULATE FPS
+                new_frame_time = time.time()
+                fps = 1/(new_frame_time-prev_frame_time)
+                prev_frame_time = new_frame_time
+                fps = int(fps)
+                print(f'FPS :  {str(fps)}')
+                #Draw Rectangle x1 300 x2 340 y1 0 y2 480 
+                cv2.rectangle(frame, (230, 49), (340, 431), (0, 255, 0), 2)
+                for result in results:
+                    ripness, boxes = find_strawberry(result)
+                    if boxes is not None and ripness is not None:
+                        x1, x2 = boxes[0], boxes[2]
+                        print(f'Ripness: {ripness} | x1 : {x1} | x2 : {x2}')
 
 
-    cap.release()
-    cv2.destroyAllWindows()
+                cv2.imshow(window_title, frame)
+                keyCode = cv2.waitKey(10) & 0xFF
+                # Stop the program on the ESC key or 'q'
+                if keyCode == 27 or keyCode == ord('q'):
+                    break
+        except Exception as e:
+            print(e)
+        finally:
+            video_capture.release()
+            cv2.destroyAllWindows()
+    else:
+        print("Error: Unable to open camera")
+
 
 if __name__ == "__main__":
-    main()
+    print('Load Model...')
+    model = YOLO('model/segment/best.engine', task='segment')
+    show_camera(model)
