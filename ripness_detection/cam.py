4d37548ba32491e808284284759a2f8c2390e0d9
Tanapoowpat <tanapoowapat@gmail.com>
2024-02-10 17:39:34 +0700

    modified:   ripness_detection/cam.py 	modified:   ripness_detection/usb-camera-gst.py

--- ripness_detection/cam.py
+++ ripness_detection/cam.py
@@ -27,15 +27,15 @@ def show_camera():
                 _, frame = video_capture.read()
                 #Bitwise And Mask
                 frame = cv2.bitwise_and(frame, mask)
-                
+
                 #CALCULATE FPS
                 new_frame_time = time.time()
                 fps = 1/(new_frame_time-prev_frame_time)
                 prev_frame_time = new_frame_time
                 fps = int(fps)
                 print(f'FPS :  {str(fps)}')
-                #Draw Rectangle x1 300 x2 340 y1 0 y2 480 
-                cv2.rectangle(frame, (300,0), (340,480), (0, 255, 0), 2)
+                #Draw Rectangle x1 230 x2 340 y1 49 y2 431 
+                cv2.rectangle(frame, (230,49), (340,431), (0, 255, 0), 2)
                 cv2.imshow(window_title, frame)
                 keyCode = cv2.waitKey(10) & 0xFF
                 # Stop the program on the ESC key or 'q'
