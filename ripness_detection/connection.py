77f78dd687c89d230a6dbf6ffd96586e86e39514
Tanapoowpat <tanapoowapat@gmail.com>
2024-02-12 01:06:39 +0700

    modified:   ripness_detection/connection.py

--- ripness_detection/connection.py
+++ ripness_detection/connection.py
@@ -22,6 +22,6 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("192.168.137.41", 1883, 60)
+client.connect("localhost", 1883, 60)
 
 client.loop_forever()
