6084d8eee792222f1f43e4aff49f42537ca3906a
Tanapoowpat <tanapoowapat@gmail.com>
2024-02-13 00:55:25 +0700

    modified:   ripness_detection/arduio_connect.py 	modified:   ripness_detection/process.py

--- ripness_detection/arduio_connect.py
+++ ripness_detection/arduio_connect.py
@@ -8,13 +8,16 @@ def send_data_to_arduino(py, arduino):
     time.sleep(0.1)
 
 def send_data(py):
-    arduino_port = '/dev/ttyACM0'
-    baud_rate = 9600
-    arduino = serial.Serial(port=arduino_port, baudrate=baud_rate, timeout=1)
-    
-    send_data_to_arduino(py, arduino)
-    while True:
-        print('Receiving data from arduino...')
-        received_data = arduino.readline().decode('utf8').strip()
-        if received_data == "succeed":
-            break
+    if py > 21 or py < 11:
+        print('Invalid data')
+    else:
+        arduino_port = '/dev/ttyACM0'
+        baud_rate = 9600
+        arduino = serial.Serial(port=arduino_port, baudrate=baud_rate, timeout=1)
+        
+        send_data_to_arduino(py, arduino)
+        while True:
+            print('Receiving data from arduino...')
+            received_data = arduino.readline().decode('utf8').strip()
+            if received_data == "succeed":
+                break
