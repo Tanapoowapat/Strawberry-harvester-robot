a4afaddaffdffd2f1a39c952a5ad032c993651cd
Tanapoowpat <tanapoowapat@gmail.com>
2024-02-17 22:59:54 +0700

    modified:   ripness_detection/image_processing.py

--- ripness_detection/image_processing.py
+++ ripness_detection/image_processing.py
@@ -100,7 +100,7 @@ def show_camera(model, ripeness):
             cv2.imshow(WINDOW_TITLE, frame)
             keyCode = cv2.waitKey(10) & 0xFF
             if keyCode == 27 or keyCode == ord('q'):
-                #send_data_to_arduino("finish")
+                send_data_to_arduino("stop")
                 MOTOR = False
                 break
             
