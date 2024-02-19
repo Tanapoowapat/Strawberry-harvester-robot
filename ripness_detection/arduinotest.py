3ddce48b14e7592e001b371c127e82cd4e044959
Tanapoowpat <tanapoowapat@gmail.com>
2024-02-20 02:36:41 +0700

    modified:   ripness_detection/arduinotest.py

--- ripness_detection/arduinotest.py
+++ ripness_detection/arduinotest.py
@@ -2,6 +2,22 @@ import threading
 import time
 from arduio_connect import send_data_to_arduino, arduino_receive_callback, received_data_queue, arduino
 
+import cv2
+
+PIPELINE = " ! ".join([
+    "v4l2src device=/dev/video0",
+    "video/x-raw, width=640, height=480, framerate=30/1",
+    "videoconvert",
+    "video/x-raw, format=(string)BGR",
+    "appsink"
+])
+#open camera
+def open_camera(video_capture):
+    print('Start Reading Camera...')
+    video_capture = cv2.VideoCapture(PIPELINE, cv2.CAP_GSTREAMER)
+    return video_capture
+
+
 
 def main():
 
