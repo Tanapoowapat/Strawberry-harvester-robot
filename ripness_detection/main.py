abf66d96abd421bbad582b39ea132ab1789cb437
Tanapoowpat <tanapoowapat@gmail.com>
2024-03-04 21:49:07 +0700

    modified:   ripness_detection/main.py

--- ripness_detection/main.py
+++ ripness_detection/main.py
@@ -18,7 +18,7 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     received_data = msg.payload
     json_data = json.loads(received_data)
-    #print(f'Server sand : {json_data} Type: {type(json_data)}\n')
+    print(f'Server sand : {json_data} Type: {type(json_data)}\n')
     ch = json_data['ripeness']
 
     if ch == 'q' :
@@ -26,26 +26,26 @@ def on_message(client, userdata, msg):
         client.publish("sys_nano/status", 3)
         client.disconnect()
         # sys.exit()
-        if 'ripeness' in json_data:
-            client.publish("sys_nano/status", 2)
-            ripeness_level = None
-            ripeness = json_data['ripeness']
-        if ripeness == 1:
-            ripeness_level = 'Unripe' 
-        elif ripeness == 2:
-            ripeness_level = 'SmallRipe' 
-        elif ripeness == 3:
-            ripeness_level = 'MediumRipe' 
-        elif ripeness == 4:
-            ripeness_level = 'Ripe' 
-        elif ripeness == 5:
-            ripeness_level = 'FullRipe' 
-        else:
-            print('unknow input')
+    if 'ripeness' in json_data:
+        client.publish("sys_nano/status", 2)
+        ripeness_level = None
+        ripeness = json_data['ripeness']
+    if ripeness == 1:
+        ripeness_level = 'Unripe' 
+    elif ripeness == 2:
+        ripeness_level = 'SmallRipe' 
+    elif ripeness == 3:
+        ripeness_level = 'MediumRipe' 
+    elif ripeness == 4:
+        ripeness_level = 'Ripe' 
+    elif ripeness == 5:
+        ripeness_level = 'FullRipe' 
+    else:
+        print('unknow input')
 
-        if ripeness_level is not None:
-            print(f'{ripeness_level}')
-            start_process(ripeness_level)
+    if ripeness_level is not None:
+        print(f'{ripeness_level}')
+        start_process(ripeness_level)
 
         #threading.Thread(target=gpio_loop_and_sleep).start()
     else :
